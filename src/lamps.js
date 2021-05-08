16801c0ee7a07a1def3985bfcd8b2ad647170338
spectraldoy <littleguyisfum@gmail.com>
2021-05-09 03:18:21 +0530

    openai works

--- src/lamps.js
+++ src/lamps.js
@@ -2,12 +2,35 @@ import React from "react";
 import "./App.css";
 import { useState } from "react";
 
+const OpenAI = require('openai-api');
+// apparently we shouldn't include the key directly in the file
+const OPENAI_API_KEY = "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";
+const openai = new OpenAI(OPENAI_API_KEY);
+
+async function GPTOut(prompt_) {
+    const gptResponse = await openai.complete({
+        engine: 'davinci',
+        prompt: prompt_,
+        maxTokens: 12,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ['\n', "testing"]
+    });
+            
+    return gptResponse.data.choices.text;
+};
+
 const Lamps = () => {
   const [lampOn, turnOn] = useState(false);
   return (
     <div className="lamp-wrapper">
       {!lampOn ? (
-        <div onClick={() => turnOn(true)}>
+        <div onClick={() => {turnOn(true); GPTOut("Come up with a recipe using mushrooms.");}}>
           <img src="/LampOff.png" alt="lamp off" />
         </div>
       ) : (
