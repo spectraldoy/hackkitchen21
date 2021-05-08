aad9cb9ab94a7904060b1def1c08456f3859234c
spectraldoy <littleguyisfum@gmail.com>
2021-05-09 04:07:16 +0530

    gpt starts to work

--- src/lamps.js
+++ src/lamps.js
@@ -8,11 +8,20 @@ const OpenAI = require('openai-api');
 const OPENAI_API_KEY = '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';
 const openai = new OpenAI(OPENAI_API_KEY);
 
-async function GPTOut(prompt_) {
+
+function changeBGColor(color) {
+  document.body.style.background = color;
+}
+
+const Lamps = () => {
+  const [lampOn, turnOn] = useState(false);
+  const [recipeData, changeRecipeData] = useState('');
+
+  async function GPTOut(prompt_) {
     const gptResponse = await openai.complete({
         engine: 'davinci',
         prompt: prompt_,
-        maxTokens: 12,
+        maxTokens: 25,
         temperature: 0.9,
         topP: 1,
         presencePenalty: 0,
@@ -24,21 +33,17 @@ async function GPTOut(prompt_) {
     });
     console.log(gptResponse.data)
             
-    return console.log(gptResponse.data);
-};
+    changeRecipeData(gptResponse.data.choices[0].text);
+    console.log(recipeData);
+  };
 
-function changeBGColor(color) {
-  document.body.style.background = color;
-}
 
-const Lamps = () => {
-  const [lampOn, turnOn] = useState(false);
   return (
     <div className="lamp-wrapper">
       {!lampOn ? (
         <div onClick={() => {
             turnOn(true); 
-            GPTOut("Come up with a recipe for mushroom soup.");
+            GPTOut("Come up with a recipe for pizza.");
             changeBGColor("white");
           }}>
           <img className="Lamp" src="/LampOff.png" alt="lamp off" />
@@ -49,6 +54,9 @@ const Lamps = () => {
             changeBGColor("#282c34");
           }}>
           <img className="Lamp" src="/LampOn.png" alt="lamp on" />
+          <p>
+            {recipeData}
+          </p>
         </div>
       )}
     </div>
