58a3a6b28f38a74f2f1b0c79747f2ab16d9ded38
Prateek Sane <61072843+PrateekSane@users.noreply.github.com>
2021-05-08 14:34:19 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import GPTOut from './gpt';
+import React from "react";
+import "./App.css";
+import Lamp from "./lamps";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="main-text">Recipe Lamp</p>
+      <Lamp />
     </div>
   );
 }
